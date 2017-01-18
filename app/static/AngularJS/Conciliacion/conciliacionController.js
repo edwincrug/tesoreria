57d43b9a29bf41018b6b3bd684e77796684017e1
bigpada <anta0101@hotmail.com>
2017-01-18 13:15:27 -0600

    conciliacion

--- app/static/AngularJS/Conciliacion/conciliacionController.js
+++ app/static/AngularJS/Conciliacion/conciliacionController.js
@@ -1,4 +1,4 @@
-registrationModule.controller('conciliacionController', function ($scope, $rootScope, $location, localStorageService, filtrosRepository,alertFactory) {
+registrationModule.controller('conciliacionController', function ($scope, $rootScope, $location, localStorageService,alertFactory) {
 
 	// ****************** Se guarda la información del usuario en variable userData
 	$rootScope.userData = localStorageService.get('userData');
@@ -8,12 +8,18 @@ registrationModule.controller('conciliacionController', function ($scope, $rootS
 
 		$scope.dato = "llllll";
 
-		$scope.aboContaBanco = [{"fecha":"30/01/2017","tipoPol":"UNI-EGRESOS","noPol":"728","concepto":"3210 - TANIA ANGELICA RAMIREZ PALMA","total":"47000"},
-					   			{"fecha":"30/01/2017","tipoPol":"UNI-EGRESOS","noPol":"728","concepto":"3210 - TANIA ANGELICA RAMIREZ PALMA","total":"47000"},
-					   			{"fecha":"30/01/2017","tipoPol":"UNI-EGRESOS","noPol":"728","concepto":"3210 - TANIA ANGELICA RAMIREZ PALMA","total":"47000"}];
+        $scope.totalAboContaBanco = 0;
+        $scope.totalAboBancoConta = 0;
+        $scope.totalCargoContaBanco = 0;
+        $scope.totalCargoBancoConta = 0;
 
 
+		$scope.aboContaBanco = [{"fecha":"13/01/2017","tipoPol":"UNI-EGRESOS","noPol":"728","concepto":"3210 - TANIA ANGELICA RAMIREZ PALMA","total":"47000"},
+					   			{"fecha":"07/01/2017","tipoPol":"UNI-EGRESOS","noPol":"821","concepto":"8298 - LAURA CASTILLO ALVARADO","total":"800"},
+					   			{"fecha":"21/01/2017","tipoPol":"UNI-EGRESOS","noPol":"914","concepto":"5392 - MONICA LOPEZ LEON","total":"2300"}];
 
+	    $scope.cargBancoConta = [{"fecha":"13/01/2017","tipoPol":"UNI-EGRESOS","noPol":"728","concepto":"3210 - TANIA ANGELICA RAMIREZ PALMA","total":"47000"},					   			
+					   			{"fecha":"21/01/2017","tipoPol":"UNI-EGRESOS","noPol":"914","concepto":"5392 - MONICA LOPEZ LEON","total":"23000"}];
 
 	}
 });
