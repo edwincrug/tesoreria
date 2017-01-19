976682b09217ad04a8f7cfe641d0dd028235f1ae
Fernando Alvarado Luna <falvaradoluna@gmail.com>
2017-01-19 16:50:45 -0600

    FAL
    grid apariencia

--- app/static/AngularJS/ControlDepositos/controlDepositosController.js
+++ app/static/AngularJS/ControlDepositos/controlDepositosController.js
@@ -22,7 +22,38 @@ registrationModule.controller('controlDepositosController',  function ($scope, $
     }
 
 
-     $scope.gridOptions = {
+  $scope.gridDepositos = { enableRowSelection: true, enableRowHeaderSelection: false };
+
+  $scope.gridDepositos.columnDefs = [
+    { name: 'id' },
+    { name: 'name'},
+    { name: 'age', displayName: 'Age (not focusable)', allowCellFocus : false },
+    { name: 'address.city' }
+  ];
+
+  $scope.gridDepositos.multiSelect = false;
+  $scope.gridDepositos.modifierKeysToMultiSelect = false;
+  $scope.gridDepositos.noUnselect = true;
+  $scope.gridDepositos.onRegisterApi = function( gridApi ) {
+    $scope.gridApi = gridApi;
+  };
+
+  $scope.toggleRowSelection = function() {
+    $scope.gridApi.selection.clearSelectedRows();
+    $scope.gridDepositos.enableRowSelection = !$scope.gridDepositos.enableRowSelection;
+    $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.OPTIONS);
+  };
+
+  $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+    .success(function(data) {
+      $scope.gridDepositos.data = data;
+
+      // $interval whilst we wait for the grid to digest the data we just gave it
+      $interval( function() {$scope.gridApi.selection.selectRow($scope.gridDepositos.data[0]);}, 0, 1);
+    });
+
+
+  $scope.gridDocumentos = {
     enableRowSelection: true,
     enableSelectAll: true,
     selectionRowHeaderWidth: 35,
@@ -30,22 +61,27 @@ registrationModule.controller('controlDepositosController',  function ($scope, $
     showGridFooter:true
   };
 
-  $scope.gridOptions.columnDefs = [
+  $scope.gridDocumentos.columnDefs = [
     { name: 'id' },
     { name: 'name'},
     { name: 'age', displayName: 'Age (not focusable)', allowCellFocus : false },
     { name: 'address.city' }
   ];
 
-  $scope.gridOptions.multiSelect = true;
+  $scope.gridDocumentos.multiSelect = true;
 
   $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
     .success(function(data) {
-      $scope.gridOptions.data = data;
+      $scope.gridDocumentos.data = data;
       $timeout(function() {
         if($scope.gridApi.selection.selectRow){
-          $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
+          $scope.gridApi.selection.selectRow($scope.gridDocumentos.data[0]);
         }
       });
     });
+
+
+
+
+
 });
