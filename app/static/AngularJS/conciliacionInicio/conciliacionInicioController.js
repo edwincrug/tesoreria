92f51c52c30c1082a77b36a110d4c87800498489
GibranQC <gibranqc9131@gmail.com>
2017-01-19 17:47:16 -0600

    Filtros

--- app/static/AngularJS/conciliacionInicio/conciliacionInicioController.js
+++ app/static/AngularJS/conciliacionInicio/conciliacionInicioController.js
@@ -1,4 +1,4 @@
-registrationModule.controller('conciliacionInicioController', function ($scope, $rootScope, $location, localStorageService, conciliacionInicioRepository,alertFactory) {
+registrationModule.controller('conciliacionInicioController', function ($scope, $rootScope, $location, localStorageService,filtrosRepository, conciliacionInicioRepository,alertFactory) {
 
 	// ****************** Se guarda la información del usuario en variable userData
 	$rootScope.userData = localStorageService.get('userData');
@@ -6,6 +6,11 @@ registrationModule.controller('conciliacionInicioController', function ($scope, 
 	
 	$scope.init = function(){
 		$scope.calendario();
+		 $scope.getEmpresa(1);
+		 $scope.getBancos(1);
+		 $scope.getCuentaBanco(1)
+		$scope.getClaveBanco(1)
+		$scope.getCuentacontable(1)		 
 		// if($rootScope.userData == null){
 		// 	location.href = '/';
 		// 	alertFactory.warning('Inicie Sesión')
@@ -26,4 +31,48 @@ registrationModule.controller('conciliacionInicioController', function ($scope, 
             format: "dd/mm/yyyy"
         });
     }
+
+    $scope.getEmpresa = function(idEmpresa) {
+        filtrosRepository.getEmpresas(idEmpresa).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.empresaUsuario = result.data;
+            }
+        });
+    }
+
+    $scope.getBancos = function(idBanco) {
+        filtrosRepository.getBancos(idBanco).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.bancoEmpresa = result.data;
+            }
+        });
+    }
+
+    $scope.getCuentaBanco = function(idCuentaBanco) {
+        filtrosRepository.getCuentaBanco(idCuentaBanco).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.bancoCuenta = result.data;
+            }
+        });
+    }
+
+    $scope.getClaveBanco = function(idClaveBanco) {
+        filtrosRepository.getClaveBanco(idClaveBanco).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.bancoClave = result.data;
+            }
+        });
+    }
+
+    $scope.getCuentacontable = function(idCuentacontable) {
+        filtrosRepository.getCuentacontable(idCuentacontable).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.cuentacontable = result.data;
+            }
+        });
+    }
+
+    
+
+
 });
