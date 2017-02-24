dd6668dd697e10a50201746e711faf2988860434
Fernando Alvarado Luna <falvaradoluna@gmail.com>
2017-02-24 13:30:18 -0600

    FAL
    ng.edit

--- app/static/AngularJS/registrationModule.js
+++ app/static/AngularJS/registrationModule.js
@@ -5,7 +5,7 @@
 // -- Modificó: 
 // -- Fecha: 
 // -- =============================================
-var registrationModule = angular.module("registrationModule", ["ngRoute", "LocalStorageModule", 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping', 'ui.grid.pinning'])
+var registrationModule = angular.module("registrationModule", ["ngRoute", "LocalStorageModule", 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping', 'ui.grid.pinning','ui.grid.edit','ngTouch'])
     .config(function($routeProvider, $locationProvider) {
 
         /*cheange the routes*/
