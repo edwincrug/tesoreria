965a44bccee978c1800e50be2371d19ee9505701
GibranQC <gibranqc9131@gmail.com>
2017-02-27 15:58:36 -0600

    RolesUsuarios

--- app/static/AngularJS/Main/mainControlller.js
+++ app/static/AngularJS/Main/mainControlller.js
@@ -1,21 +1,29 @@
-registrationModule.controller('mainController', function ($scope, $rootScope, $location, localStorageService,alertFactory) {
+registrationModule.controller('mainController', function($scope, $rootScope, $location, localStorageService, alertFactory) {
 
-	$rootScope.userData = localStorageService.get('userData');
+    $rootScope.userData = localStorageService.get('userData');
 
-	$scope.init = function(){
-		$rootScope.mostrarMenu = 1;
-		/*if($rootScope.userData == null){
-			$rootScope.mostrarMenu = 0;
-			alertFactory.warning('Inicie Sesión')
-		}else{
-			alertFactory.success('Bienvenido '+ $rootScope.userData.nombreUsuario)
-			$rootScope.mostrarMenu = 1;
-		}*/
-	}
-	// ************** Función para cerrar sesión
-	// ************** NOTA se limpian todos los localStorage utilizados
-	 $scope.salir = function(){
-	 	alertFactory.warning('Hasta luego '+ $rootScope.userData.nombreUsuario)
+    $scope.init = function() {
+            $rootScope.mostrarMenu = 1;
+            if ($rootScope.userData.idPerfil == 4) {
+            	$rootScope.controlDepositosAcceso = 0;
+                $rootScope.conciliacionAccesso = 1;          
+                console.log('Administrador Control Depositos')
+            } else {
+                $rootScope.controlDepositosAcceso = 1;
+                $rootScope.conciliacionAccesso = 0;
+            }
+            /*if($rootScope.userData == null){
+            	$rootScope.mostrarMenu = 0;
+            	alertFactory.warning('Inicie Sesión')
+            }else{
+            	alertFactory.success('Bienvenido '+ $rootScope.userData.nombreUsuario)
+            	$rootScope.mostrarMenu = 1;
+            }*/
+        }
+        // ************** Función para cerrar sesión
+        // ************** NOTA se limpian todos los localStorage utilizados
+    $scope.salir = function() {
+        alertFactory.warning('Hasta luego ' + $rootScope.userData.nombreUsuario)
         localStorageService.clearAll('userData');
         location.href = '/';
     }
--- app/static/AngularJS/login/loginController.js
+++ app/static/AngularJS/login/loginController.js
@@ -11,13 +11,25 @@ registrationModule.controller('loginController', function($scope, $rootScope, $l
         loginRepository.getPermisos(usuario, contrasena).then(function(result) {
             if (result.data.length > 0) {
                 $scope.login = result.data[0];
-                localStorageService.set('userData', $scope.login);
-                alertFactory.warning('Bienvenido a Talleres: ' + result.data[0].nombreUsuario);
-                location.href = '/conciliacionInicio';
-            }else{
+                if ($scope.login.idPerfil == 4) {
+                    $rootScope.controlDepositosAcceso = 1;
+                    $rootScope.conciliacionAccesso = 0;
+                    alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0].nombreUsuario);
+                    location.href = '/conciliacionInicio';
+                    localStorageService.set('userData', $scope.login);
+                } else {
+                    $rootScope.controlDepositosAcceso = 0;
+                    $rootScope.conciliacionAccesso = 1;
+                    alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0].nombreUsuario);
+                    location.href = '/controlDepositos';
+                    localStorageService.set('userData', $scope.login);
+                    
+                }                
+
+            } else {
                 alertFactory.info('Valide el usuario y/o contraseña');
             }
-      
+
         });
     }
 });
