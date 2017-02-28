3dea0320178d5701f83b400c3fabc857e51970b4
hhipolito <hhipolito@bism.com.mx>
2017-02-28 17:26:08 -0600

    insert referncias
    inserta referencias a depositos

--- app/static/AngularJS/ControlDepositos/controlDepositosController.js
+++ app/static/AngularJS/ControlDepositos/controlDepositosController.js
@@ -1,8 +1,14 @@
-registrationModule.controller('controlDepositosController', function($scope, $rootScope, $location, localStorageService, filtrosRepository,alertFactory, $http, $log, $timeout, uiGridConstants) {
+registrationModule.controller('controlDepositosController', function($scope, $rootScope, $location, localStorageService, filtrosRepository, alertFactory, $http, $log, $timeout, uiGridConstants, controlDepositosRepository) {
 
     // ****************** Se guarda la información del usuario en variable userData
     $rootScope.userData = localStorageService.get('userData');
     $scope.idUsuario = 15;
+
+    $scope.filtros = {
+        idEmpresa: '',
+        idSucursal: '',
+        idDepartamento: ''
+    };
 
     $scope.init = function() {
         $scope.getEmpresa($scope.idUsuario);
@@ -22,7 +28,7 @@ registrationModule.controller('controlDepositosController', function($scope, $ro
         $scope.carteraTotal = 0;
         //$scope.getDepositosBancosNoReferenciados(1,1,'10/11/2015','31/12/2015');
         //$scope.getCarteraVencida(31996,4,12,67,'10/11/2015','31/12/2015');
-        $scope.filtroscheck = {cargo : 1 };
+        $scope.filtroscheck = { cargo: 1 };
     }
 
     $scope.calendario = function() {
@@ -46,19 +52,19 @@ registrationModule.controller('controlDepositosController', function($scope, $ro
     }
 
     $scope.empresaSeleccionada = function(idEmpresa) {
-           
-                $scope.activarBanco = true;
-                $scope.activarSucursal = true;
-                $scope.getSucursales(15,5);
-                $scope.getBancos(idEmpresa);
 
-        }
+        $scope.activarBanco = true;
+        $scope.activarSucursal = true;
+        $scope.getSucursales(15, 5);
+        $scope.getBancos(idEmpresa);
+
+    }
 
 
     $scope.getSucursales = function(idEmpresa) {
 
         $scope.activarSucursal = false;
-        filtrosRepository.getSucursales($scope.idUsuario,idEmpresa).then(function(result) {
+        filtrosRepository.getSucursales($scope.idUsuario, idEmpresa).then(function(result) {
             if (result.data.length > 0) {
                 $scope.sucursalesUsuario = result.data;
             }
@@ -67,7 +73,7 @@ registrationModule.controller('controlDepositosController', function($scope, $ro
 
     $scope.getDepartamentos = function(idSucursal) {
         $scope.activarDepartamento = false;
-        filtrosRepository.getDepartamentos($scope.idUsuario,idSucursal).then(function(result) {
+        filtrosRepository.getDepartamentos($scope.idUsuario, idSucursal).then(function(result) {
             if (result.data.length > 0) {
                 $scope.departamentosUsuario = result.data;
             }
@@ -76,49 +82,49 @@ registrationModule.controller('controlDepositosController', function($scope, $ro
 
     $scope.getBancos = function(idEmpresa) {
         $scope.activarBanco = false;
-        $scope.cuentaBancaria = 
-        filtrosRepository.getBancos(idEmpresa).then(function(result) {
-            if (result.data.length > 0) {
-                $scope.bancoEmpresa = result.data;
-            }
-        });
+        $scope.cuentaBancaria =
+            filtrosRepository.getBancos(idEmpresa).then(function(result) {
+                if (result.data.length > 0) {
+                    $scope.bancoEmpresa = result.data;
+                }
+            });
     }
 
-     $scope.getCuentas = function(idBanco,idEmpresa) {
-        
+    $scope.getCuentas = function(idBanco, idEmpresa) {
+
         $scope.activarCuenta = false;
         $scope.cuentaBancaria = [];
-        filtrosRepository.getCuenta(idBanco,idEmpresa).then(function(result) {
+        filtrosRepository.getCuenta(idBanco, idEmpresa).then(function(result) {
             if (result.data.length > 0) {
                 $scope.cuentaBancaria = result.data;
             }
         });
     }
 
-    $scope.getCarteraVencida = function(cliente,empresa,sucursal,departamento,fechaIni,fechaFin){
+    $scope.getCarteraVencida = function(cliente, empresa, sucursal, departamento, fechaIni, fechaFin) {
         $scope.gridCartera.data = [];
 
-      filtrosRepository.getCartera(cliente,empresa,sucursal,departamento,fechaIni,fechaFin).then(function(result) {
+        filtrosRepository.getCartera(cliente, empresa, sucursal, departamento, fechaIni, fechaFin).then(function(result) {
             if (result.data.length > 0) {
                 $scope.gridCartera.data = result.data;
             }
         });
     }
 
-     $scope.getDepositosBancosNoReferenciados = function(empresa,cuenta,fechaIni,fechaFin){
+    $scope.getDepositosBancosNoReferenciados = function(empresa, cuenta, fechaIni, fechaFin) {
         $scope.gridDocumentos.data = [];
-      filtrosRepository.getDepositosNoReferenciados(empresa,cuenta,fechaIni,fechaFin).then(function(result) {
+        filtrosRepository.getDepositosNoReferenciados(empresa, cuenta, fechaIni, fechaFin).then(function(result) {
             if (result.data.length > 0) {
-               $scope.gridDocumentos.data = result.data;
+                $scope.gridDocumentos.data = result.data;
             }
         });
     }
 
-     $scope.getCalendartios = function(idBanco,idEmpresa) {
-        
+    $scope.getCalendartios = function(idBanco, idEmpresa) {
+
         $scope.activarCuenta = false;
-        
-        filtrosRepository.getCuenta(idBanco,idEmpresa).then(function(result) {
+
+        filtrosRepository.getCuenta(idBanco, idEmpresa).then(function(result) {
             if (result.data.length > 0) {
                 $scope.cuentaBancaria = result.data;
             }
@@ -126,134 +132,141 @@ registrationModule.controller('controlDepositosController', function($scope, $ro
     }
 
     $scope.empresaVacia = function() {
-            
-            $scope.filtros.idCuenta = null;
-            $scope.filtros.fechaInicioDeposito = null;
-            $scope.filtros.fechaFinDeposito = null;
-            $scope.filtros.idDepartamento = null;
-            $scope.filtros.fechaInicioDeposito = null;
-            $scope.filtros.fechaFinDeposito = null;
-            $scope.activarCuenta = true;
-            $scope.activarFechaIniDeposito = true;
-            $scope.activarFechaFinDeposito = true;
-            $scope.activarDepartamento = true;
-            $scope.activarFechaIniCartera = true;
-            $scope.activarFechaFinCartera = true;
-            $scope.activarBuscarDepositos = true;
-            $scope.activarBuscarCartera = true;
-            $scope.gridCartera.data = [];
+
+        $scope.filtros.idCuenta = null;
+        $scope.filtros.fechaInicioDeposito = null;
+        $scope.filtros.fechaFinDeposito = null;
+        $scope.filtros.idDepartamento = null;
+        $scope.filtros.fechaInicioDeposito = null;
+        $scope.filtros.fechaFinDeposito = null;
+        $scope.activarCuenta = true;
+        $scope.activarFechaIniDeposito = true;
+        $scope.activarFechaFinDeposito = true;
+        $scope.activarDepartamento = true;
+        $scope.activarFechaIniCartera = true;
+        $scope.activarFechaFinCartera = true;
+        $scope.activarBuscarDepositos = true;
+        $scope.activarBuscarCartera = true;
+        $scope.gridCartera.data = [];
         $scope.gridDocumentos.data = [];
-        }
+    }
 
     $scope.gridCartera = {
-      enableColumnResize: true,
-                enableRowSelection: true,
-                enableGridMenu: true,
-                enableFiltering: true,
-                enableGroupHeaderSelection: false,
-                treeRowHeaderAlwaysVisible: true,
-                showColumnFooter: true,
-                showGridFooter: true,
-                height: 900,
-                cellEditableCondition: function($scope) {
-                    return $scope.row.entity.seleccionable;
-                }
+        enableColumnResize: true,
+        enableRowSelection: true,
+        enableGridMenu: true,
+        enableFiltering: true,
+        enableGroupHeaderSelection: false,
+        treeRowHeaderAlwaysVisible: true,
+        showColumnFooter: true,
+        showGridFooter: true,
+        height: 900,
+        cellEditableCondition: function($scope) {
+            return $scope.row.entity.seleccionable;
+        }
     };
 
-  $scope.gridCartera.columnDefs = [
-    { name: 'nombreSucursal', width: '10%',displayName: 'Sucursal'},
-    { name: 'nombreDepartamento', width: '10%', displayName: 'Departamento'},
-    { name: 'folio', width: '10%',displayName: 'Factura'},
-    { name: 'fecha', width: '10%',displayName: 'fecha'},
-    { name: 'nombreCliente', width: '30%',displayName: 'Cliente'},
-    { name: 'importe', width: '10%',displayName: 'Importe',cellFilter: 'currency'},
-    { name: 'saldo', width: '10%',displayName: 'Saldo',cellFilter: 'currency'},
+    $scope.gridCartera.columnDefs = [
+        { name: 'nombreSucursal', width: '10%', displayName: 'Sucursal' },
+        { name: 'nombreDepartamento', width: '10%', displayName: 'Departamento' },
+        { name: 'folio', width: '10%', displayName: 'Factura' },
+        { name: 'fecha', width: '10%', displayName: 'fecha' },
+        { name: 'nombreCliente', width: '30%', displayName: 'Cliente' },
+        { name: 'importe', width: '10%', displayName: 'Importe', cellFilter: 'currency' },
+        { name: 'saldo', width: '10%', displayName: 'Saldo', cellFilter: 'currency' },
 
-  ];
+    ];
 
-  $scope.gridCartera.multiSelect = true;
+    $scope.gridCartera.multiSelect = true;
 
-   $scope.gridDocumentos = { 
-    enableColumnResize: true,
-                enableRowSelection: true,
-                enableGridMenu: true,
-                enableFiltering: true,
-                enableGroupHeaderSelection: false,
-                treeRowHeaderAlwaysVisible: true,
-                showColumnFooter: true,
-                showGridFooter: true,
-                height: 900,
-                cellEditableCondition: function($scope) {
-                    return $scope.row.entity.seleccionable;
-                }
-     };
+    $scope.gridDocumentos = {
+        enableColumnResize: true,
+        enableRowSelection: true,
+        enableGridMenu: true,
+        enableFiltering: true,
+        enableGroupHeaderSelection: false,
+        treeRowHeaderAlwaysVisible: true,
+        showColumnFooter: true,
+        showGridFooter: true,
+        height: 900,
+        cellEditableCondition: function($scope) {
+            return $scope.row.entity.seleccionable;
+        }
+    };
 
-  $scope.gridDocumentos.columnDefs = [
-    { name: 'banco',displayName: 'Banco', width: '10%' },
-    { name: 'idBmer',displayName: 'Cons', width: '5%'},
-    { name: 'referencia',displayName: 'Referencia', width: '15%' },
-    { name: 'concepto' ,displayName: 'Concepto', width: '15%'},
-    { name: 'fechaOperacion',displayName: 'Fecha', width: '10%',type: 'date', cellFilter: 'date:"dd/MM/yyyy"'},
-    { name: 'cargo',displayName: 'Cargo', width: '10%', cellFilter: 'currency' },
-    { name: 'abono' ,displayName: 'Abono', width: '10%', cellFilter: 'currency'},
-    { name: 'observaciones'
-    ,displayName: 'Observaciones'
-    ,width: '25%'
-    ,cellEditableCondition:true
+    $scope.gridDocumentos.columnDefs = [
+        { name: 'banco', displayName: 'Banco', width: '10%' },
+        { name: 'idBmer', displayName: 'Cons', width: '5%' },
+        { name: 'referencia', displayName: 'Referencia', width: '15%' },
+        { name: 'concepto', displayName: 'Concepto', width: '15%' },
+        { name: 'fechaOperacion', displayName: 'Fecha', width: '10%', type: 'date', cellFilter: 'date:"dd/MM/yyyy"' },
+        { name: 'cargo', displayName: 'Cargo', width: '10%', cellFilter: 'currency' },
+        { name: 'abono', displayName: 'Abono', width: '10%', cellFilter: 'currency' }, {
+            name: 'observaciones',
+            displayName: 'Observaciones',
+            width: '25%',
+            cellEditableCondition: true
+        }
+    ];
+
+    $scope.gridDocumentos.multiSelect = false;
+    $scope.gridDocumentos.modifierKeysToMultiSelect = false;
+    $scope.gridDocumentos.noUnselect = true;
+    $scope.gridDocumentos.onRegisterApi = function(gridApi) {
+        $scope.gridApi = gridApi;
+    };
+
+    $scope.toggleRowSelection = function() {
+        $scope.gridApi.selection.clearSelectedRows();
+        $scope.gridDocumentos.enableRowSelection = !$scope.gridDocumentos.enableRowSelection;
+        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
+    };
+
+
+    $scope.activa_calendariosDepositos = function() {
+        $scope.activarFechaIniDeposito = false;
+        $scope.activarFechaFinDeposito = false;
+        $scope.activarBuscarDepositos = false;
     }
-  ];
 
-  $scope.gridDocumentos.multiSelect = false;
-  $scope.gridDocumentos.modifierKeysToMultiSelect = false;
-  $scope.gridDocumentos.noUnselect = true;
-  $scope.gridDocumentos.onRegisterApi = function( gridApi ) {
-    $scope.gridApi = gridApi;
-  };
+    $scope.activa_calendariosCartera = function() {
+        $scope.activarFechaIniCartera = false;
+        $scope.activarFechaFinCartera = false;
+        $scope.activarBuscarCliente = false;
+        $scope.activarBuscarCartera = false;
+    }
 
-  $scope.toggleRowSelection = function() {
-    $scope.gridApi.selection.clearSelectedRows();
-    $scope.gridDocumentos.enableRowSelection = !$scope.gridDocumentos.enableRowSelection;
-    $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.OPTIONS);
-  };
+    $scope.activa_BuscarCartera = function() {
+        $scope.activarBuscarCartera = false;
+    }
 
 
- $scope.activa_calendariosDepositos = function() {
-            $scope.activarFechaIniDeposito = false;
-            $scope.activarFechaFinDeposito = false;
-            $scope.activarBuscarDepositos = false;
-        }
+    $scope.selectedDocuments = [];
+    $scope.selectedCartera = [];
 
- $scope.activa_calendariosCartera = function() {
-            $scope.activarFechaIniCartera = false;
-            $scope.activarFechaFinCartera = false;
-            $scope.activarBuscarCliente = false;
-            $scope.activarBuscarCartera = false;
-        }
+    $scope.gridDocumentos.onRegisterApi = function(gridApi) {
 
- $scope.activa_BuscarCartera = function() {
-             $scope.activarBuscarCartera = false;
-        }
-
-$scope.gridDocumentos.onRegisterApi = function(gridApi) {
-        $scope.gridApiDocumentos = gridApi;
         gridApi.selection.on.rowSelectionChanged($scope, function(row) {
             //var msg = 'row selected ' + row.isSelected;
             if (row.isSelected == true) {
                 $scope.depositoTotal = row.entity.abono;
-                
-            } 
+                $scope.selectedDocuments.shift();
+                $scope.selectedDocuments.push(row.entity);
+            } else if (row.isSelected == false) {
+                $scope.selectedDocuments.shift();
+            }
         }); //Este me dice cuales van siendo seleccionadas
 
-      
+
     };
 
-$scope.gridCartera.onRegisterApi = function(gridApi) {
-        //set gridApi on scope
-        $scope.gridApiCartera = gridApi;
+    $scope.gridCartera.onRegisterApi = function(gridApi) {
+
         gridApi.selection.on.rowSelectionChanged($scope, function(row) {
             var msg = 'row selected ' + row.isSelected;
             if (row.isSelected == true) {
                 $scope.carteraTotal = $scope.carteraTotal + parseFloat(row.entity.importe);
+                $scope.selectedCartera.push(row.entity);
             } else if (row.isSelected == false) {
                 $scope.carteraTotal = $scope.carteraTotal - parseFloat(row.entity.importe);
             }
@@ -261,5 +274,111 @@ $scope.gridCartera.onRegisterApi = function(gridApi) {
         }); //Este me dice cuales van siendo seleccionadas
     };
 
- 
+
+
+
+
+
+
+
+    $scope.guardarGrid = function() {
+
+        console.log('dffdsfsd');
+
+        var params = $scope.setParamsCreateReference($scope.selectedCartera[0]);
+        $scope.createReference(params);
+
+    };
+
+
+
+    $scope.setParamsCreateReference = function(obj) {
+
+        var params = {};
+
+        params.idEmpresa = $scope.filtros.idEmpresa;
+        params.idSucursal = $scope.filtros.idSucursal;
+        params.idDepartamento = 1; //$scope.filtros.idDepartamento;
+        params.idTipoDocumento = 1; //siempre es uno segun gibi
+        params.serie = obj.serie;
+        params.folio = obj.idDocumento;
+        params.idCliente = obj.idCliente;
+        params.idAlma = 0;
+        params.importeDocumento = obj.importe;
+        params.idTipoReferencia = 3;
+
+        return params;
+    };
+
+
+
+    $scope.setParamsInsertReferenceDetails = function(idreferencia, obj) {
+
+        var params = {};
+
+        params.idReferencia = idreferencia;
+        params.idSucursal = $scope.filtros.idSucursal;
+        params.idDepartamento = 1; //$scope.filtros.idDepartamento;
+        params.idTipoDocumento = 1;
+        params.serie = obj.serie;
+        params.folio = obj.idDocumento;
+        params.idCliente = obj.idCliente;
+        params.idAlma = 0;
+        params.importeDocumento = obj.importe;
+
+        return params;
+    };
+
+
+    $scope.createReference = function(objData) {
+
+        $scope.promise = controlDepositosRepository.createReference(objData).then(function(result) {
+            if (result.data.length > 0) {
+
+                var idRef = result.data[0].idReferencia;
+
+                for (var i = 0; i < $scope.selectedCartera.length; i++) {
+                    var params = $scope.setParamsInsertReferenceDetails(idRef, $scope.selectedCartera[i]);
+                    $scope.insertReferenceDetails(params);
+                };
+
+            } else {
+                console.log('no trajo nada');
+            }
+        }, function(error) {
+            console.log('Error');
+        });
+    };
+
+
+
+    $scope.insertReferenceDetails = function(objData) {
+
+        $scope.promise = controlDepositosRepository.insertReferenceDetails(objData).then(function(result) {
+
+            if (result.data.length > 0) {
+                console.log('ok');
+            } else {
+                console.log('no trajo nada');
+            }
+        }, function(error) {
+            console.log('Error');
+        });
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
--- app/static/AngularJS/ControlDepositos/controlDepositosRepository.js
+++ app/static/AngularJS/ControlDepositos/controlDepositosRepository.js
@@ -2,5 +2,44 @@ var controlDepositosURL = global_settings.urlCORS + 'api/controlDepositos/';
 
 registrationModule.factory('controlDepositosRepository', function($http) {
     return {
+
+        createReference: function(objData) {
+
+            return $http({
+                url: controlDepositosURL + 'createReference/',
+                method: "GET",
+                params: objData,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+
+        insertReferenceDetails: function(objData) {         
+            return $http({
+                url: controlDepositosURL + 'insertReferenceDetails/',
+                method: "GET",
+                params: objData,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+
+        testApi: function(objData) {
+
+        	console.log(objData);
+
+            return $http({
+                url: controlDepositosURL + 'testApi/',
+                method: "GET",
+                params: objData,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
+
+
     }
 });
